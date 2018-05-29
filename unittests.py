89eed3e749abb0f953d70411f101aa93214f624f
Michael Georgariou III <mpgiii@ymail.com>
2018-05-29 00:19:17 -0700

    added unit tests to make sure it works! :smile:

--- unittests.py
+++ unittests.py
@@ -47,5 +47,50 @@ class GivenAsg3Tests(unittest.TestCase):
         self.assertFalse(0 in bt)
 
 
+class MyAsg3Tests(unittest.TestCase):
+    def test_binsearchtree_constructor1(self):
+        bt1 = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+        bt2 = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+
+        self.assertListEqual(bt1.inorder(), bt2.inorder())
+        self.assertListEqual(bt1.preorder(), bt2.preorder())
+        self.assertListEqual(bt1.postorder(), bt2.postorder())
+        self.assertListEqual(bt1.levelorder(), bt2.levelorder())
+
+    def test_binsearchtree_inorder1(self):
+        bt = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+        self.assertListEqual(bt.inorder(), [-150, -100, -50, 0, 100, 2000])
+
+    def test_binsearchtree_preorder1(self):
+        bt = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+        self.assertListEqual([0, -100, -150, -50, 100, 2000], bt.preorder())
+
+    def test_binsearchtree_postorder1(self):
+        bt = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+        self.assertListEqual([-150, -50, -100, 2000, 100, 0], bt.postorder())
+
+    def test_binsearchtree_levelorder1(self):
+        bt = BinarySearchTree(contents=[0, 100, -100, -150, -50, 2000])
+        self.assertListEqual([0, -100, 100, -150, -50, 2000], bt.levelorder())
+
+    def test_binsearchtree_insert1(self):
+        bt = BinarySearchTree()
+        bt.insert(0)
+        bt.insert(-1)
+        bt.insert(1)
+        self.assertListEqual(bt.inorder(), [-1, 0, 1])
+
+    def test_binsearchtree_delete1(self):
+        bt = BinarySearchTree(contents=[0, 1, 2])
+        bt.delete(0)
+        self.assertListEqual(bt.inorder(), [1, 2])
+
+    def test_binsearchtree_contains1(self):
+        bt = BinarySearchTree(contents=[0, 1, 2])
+        self.assertTrue(0 in bt)
+        bt.delete(0)
+        self.assertFalse(0 in bt)
+
+
 if __name__ == '__main__':
     unittest.main()
