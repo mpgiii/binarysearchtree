222fc435aef7161baf6d5d0f96402e67cdc062be
Michael Georgariou III <mpgiii@ymail.com>
2018-05-28 22:32:27 -0700

    I'm close but DELETE DOESNT WORK ;-;

--- binarytreevis.py
+++ binarytreevis.py
@@ -96,27 +96,28 @@ class BinarySearchTree:
     def __insert(root, val):
         if root is None:
             return BinarySearchTree.Node(val)
-        
+
         if val < root.getVal():
             root.setLeft(BinarySearchTree.__insert(root.getLeft(), val))
         else:
             root.setRight(BinarySearchTree.__insert(root.getRight(), val))
-            
+
         return root
-
-    def minValueNode(self, node):
-        current = node
-
-        # loop down to find the leftmost leaf
-        while current.left is not None:
-            current = current.left
-
-        return current
 
     def delete(self, val):
         self.root = BinarySearchTree.__delete(self.root, val)
 
     def __delete(root, val):
+
+        def minValueNode(node):
+            current = node
+
+            # loop down to find the leftmost leaf
+            while current.left is not None:
+                current = current.left
+
+            return current
+
         if root is None:
             return None
 
@@ -129,11 +130,9 @@ class BinarySearchTree:
         else:
             if root.getLeft() is None:
                 temp = root.getRight()
-                root = None
                 return temp
             elif root.getRight() is None:
                 temp = root.getLeft()
-                root = None
                 return temp
 
             temp = minValueNode(root.getRight())
@@ -179,6 +178,19 @@ class BinarySearchTree:
                 queue.enqueue(x.getRight())
         return thelist
 
+    def levelorderwithlevels(self):
+        thelist = []
+        queue = Queue()
+        queue.enqueue(self.root)
+        while not queue.isEmpty():
+            x = queue.dequeue()
+            if x is not None:
+                thelist.append(x.getVal())
+                queue.enqueue(x.getLeft())
+                queue.enqueue(x.getRight())
+            else:
+                thelist.append(None)
+        return thelist
 
     def __str__(self):
         return "BinarySearchTree(" + repr(self.root) + ")"
@@ -196,6 +208,10 @@ class Visualization(tkinter.Frame):
 
     def buildWindow(self):
 
+        xinit = 150
+        yinit = 280
+        xratio = 0
+        yratio = 0
         cv = ScrolledCanvas(self, 600, 600, 600, 600)
         cv.pack(side=tkinter.LEFT)
         t = RawTurtle(cv)
@@ -204,7 +220,7 @@ class Visualization(tkinter.Frame):
 
         screen.setworldcoordinates(screenMin, screenMin, screenMax, screenMax)
         screen.bgcolor("white")
-        t.ht()
+        # t.hideturtle()
 
         frame = tkinter.Frame(self)
         frame.pack(side=tkinter.RIGHT, fill=tkinter.BOTH)
@@ -212,12 +228,16 @@ class Visualization(tkinter.Frame):
         tree = BinarySearchTree()
 
         def insertHandler():
-            node = nodeInput.get()
+            node = int(nodeInput.get())
             tree.insert(node)
+            t.clear()
+            redraw()
 
         def deleteHandler():
-            node = nodeInput.get()
+            node = int(nodeInput.get())
             tree.delete(node)
+            t.clear()
+            redraw()
 
         def containsHandler():
             node = nodeInput.get()
@@ -228,6 +248,89 @@ class Visualization(tkinter.Frame):
 
         def quitHandler():
             self.master.quit()
+
+        def drawline(x0, y0, x1, y1):
+            t.goto(x0, y0)
+            t.down()
+            t.goto(x1, y1)
+            t.up()
+
+        def redraw():
+            lst = tree.levelorderwithlevels()
+            lev0 = []
+            lev1 = []
+            lev2 = []
+            lev3 = []
+            lev4 = []
+            lev5 = []
+            lev6 = []
+            middle = 150
+            print(lst)
+            for i in range(len(lst)):
+                if len(lev0) < 1:
+                    lev0.append(lst[i])
+                elif len(lev1) < 2:
+                    lev1.append(lst[i])
+                elif len(lev2) < 4:
+                    lev2.append(lst[i])
+                elif len(lev3) < 8:
+                    lev3.append(lst[i])
+                elif len(lev4) < 16:
+                    lev4.append(lst[i])
+                elif len(lev5) < 32:
+                    lev5.append(lst[i])
+                elif len(lev6) < 64:
+                    lev6.append(list[i])
+            for x in lev0:
+                t.goto(150, 260)
+                t.write(x, False)
+            for i in range(len(lev1)):
+                if lev1[i] is not None:
+                    drawline(150, 260, (i+1)*100, 220)
+                    t.goto((i + 1) * 100, 220)
+                    t.write(lev1[i], False)
+            for i in range(len(lev2)):
+                if lev2[i] is not None:
+                    t.goto((i+1)*60, 180)
+                    t.write(lev2[i], False)
+            for i in range(len(lev3)):
+                if lev3[i] is not None:
+                    t.goto((i+1)*33, 140)
+                    t.write(lev3[i], False)
+            for i in range(len(lev4)):
+                if lev4[i] is not None:
+                    t.goto((i+1)*17, 100)
+                    t.write(lev4[i], False)
+            for i in range(len(lev5)):
+                if lev5[i] is not None:
+                    t.goto((i+1)*9, 60)
+                    t.write(lev5[i], False)
+            for i in range(len(lev6)):
+                if lev6[i] is not None:
+                    t.goto((i+1)*4, 20)
+                    t.write(lev6[i], False)
+
+
+
+            # yratio = xratio = 0
+            # t.goto(xinit, yinit)
+            # lst = tree.levelorder()
+            # root = lst[0]
+            # t.write(root, False)
+            # for i in range(1, len(lst)):
+            #     if lst[i] < lst[i-1]:
+            #         xratio -= 1
+            #         t.goto(120, 300 - ((yratio+1) * 20))
+            #         t.write(lst[i], False)
+            #
+            #     elif lst[i] > lst[i-1]:
+            #         yratio += 1
+            #         xratio += 1
+            #         t.goto(180, 300 - ((yratio+1) * 20))
+            #         t.write(lst[i], False)
+
+
+
 
         quitButton = tkinter.Button(frame, text="Quit", command=quitHandler)
         quitButton.pack()
